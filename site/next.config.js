f2591b0ad0b960a811c8059fbb554bc2260ce6dd
yudi4090 <yudiasrori760@gmail.com>
2025-09-09 19:10:41 +0700

    Update config file new values

--- site/next.config.js
+++ site/next.config.js
@@ -6,12 +6,12 @@ const nextConfig = {
   env: {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
+    icon: '/https://jokolo.github.io/kasm-registry/1.1/icon.png',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-registry/1.1',
   trailingSlash: true,
   images: {
     unoptimized: true,
